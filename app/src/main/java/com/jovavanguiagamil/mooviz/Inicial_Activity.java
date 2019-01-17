1448df55be81ec5b5432a8b9b44a496bae41a0de
jg <jovanyguia@gmail.com>
2019-01-17 08:39:44 +0100

    adicionando novos layout

--- app/src/main/java/com/jovavanguiagamil/mooviz/Inicial_Activity.java
+++ app/src/main/java/com/jovavanguiagamil/mooviz/Inicial_Activity.java
@@ -1,22 +1,19 @@
 package com.jovavanguiagamil.mooviz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Inicial_Activity extends AppCompatActivity {
+
+public class InicialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
     }
 
 }
